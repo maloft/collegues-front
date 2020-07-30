3c557c8d0acfc6bacd8024ffbf1cec7a3b357b8b
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-30 16:22:05 +0200

    Collègues #2

---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,6 @@
+import { Collegue } from '../models/Collegue';
+
+// tslint:disable-next-line: max-line-length
+export const collegueMock: Collegue = new Collegue('ZZZ', 'Monkey D.', 'Luffy', 'monkeyd.luffy@gmail.com', '1997-05-05', '/assets/luffy.jpg');
+
+
